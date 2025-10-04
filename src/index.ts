a85b3b4092af5a93c09310cf69db1b4baf1edeb7
vincent-the-engineer <208263664+vincent-the-engineer@users.noreply.github.com>
2025-10-04 22:00:29 +1000

    Ch1 L14:

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const PORT = 8080;
+
+app.use("/app", express.static("./src/app"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+app.get("/healthz", (req, res) => {
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.send("OK");
+})
+
