2527b4ca8d754473236e2d6325f94577d3e86634
vincent-the-engineer <208263664+vincent-the-engineer@users.noreply.github.com>
2025-11-12 00:16:27 +1100

    Ch6 L3:

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,22 @@
+envOrThrow("DB_URL");
+
 type APIConfig = {
   fileServerHits: number;
+  dbURL: string;
 };
+
+process.loadEnvFile();
 
 export const config: APIConfig = {
   fileServerHits: 0,
+  dbURL: envOrThrow("DB_URL"),
 };
+
+function envOrThrow(key: string) {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Environment variable "${key}" is not set.`);
+  }
+  return value;
+}
 
---
+++ src/db/queries/users.ts
@@ -0,0 +1,12 @@
+import { db } from "../index.js";
+import { NewUser, users } from "../schema.js";
+
+export async function createUser(user: NewUser) {
+  const [result] = await db
+    .insert(users)
+    .values(user)
+    .onConflictDoNothing()
+    .returning();
+  return result;
+}
+
